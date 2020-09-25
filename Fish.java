78e09934b143a90478305af895d7c56a480f979c
Guillermo Miranda <guillemiranda83@gmail.com>
2020-09-25 08:36:28 -0300

    nuevo metodo Surface en GIT Guille

--- Fish.java
+++ Fish.java
@@ -6,7 +6,9 @@ public class Fish implements Swimmable {
         System.out.println("OK, will dive " + howDeep + " feet");
     }
 
+
     public void surface(){
         System.out.println("Going up " + currentDepth + " feet.");
     }
-}
+
+ }
