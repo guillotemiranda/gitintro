3d17c80b9fa49abebd2b9edababcfbf90de5eb31
Guillermo Miranda <guillemiranda83@gmail.com>
2020-09-25 08:21:06 -0300

    Se agrego el metodo surface

--- Fish.java
+++ Fish.java
@@ -5,4 +5,8 @@ public class Fish implements Swimmable {
     public void dive(int howDeep){
         System.out.println("OK, will dive " + howDeep + " feet");
     }
+
+    public void surface(){
+        System.out.println("Going up " + currentDepth + " feet.");
+    }
 }
